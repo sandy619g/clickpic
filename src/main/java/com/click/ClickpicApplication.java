39b1035e1546b3a6869b61061b30bc013b90bbf3
Sandesh <sandy619g@gmail.com>
2020-07-25 16:51:31 +0530

    project setup

---
+++ src/main/java/com/click/ClickpicApplication.java
@@ -0,0 +1,13 @@
+package com.click;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickpicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickpicApplication.class, args);
+	}
+
+}
